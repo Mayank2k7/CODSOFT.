c06b0217a09554e12301c530c81a69576f52b3ad
Mayank2k7 <143578962+Mayank2k7@users.noreply.github.com>
2023-09-09 14:08:44 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Student Grade Calculator!");
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+
+            // Check if marks are within the valid range (0 to 100)
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks entered. Marks should be between 0 and 100.");
+                return;
+            }
+
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        System.out.println("Total Marks Obtained: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        
+        // Assign grades based on the average percentage
+        String grade = calculateGrade(averagePercentage);
+        System.out.println("Grade: " + grade);
+        scanner.close();
+       
+    }
+
+    public static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
